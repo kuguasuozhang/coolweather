f164a5edba866d5e3ddf2dd1aefd57b410e92235
Tony <tony@gmail.com>
2020-08-14 15:14:08 +0800

    18876311581@DESKTOP-G1TJHVC MINGW64 /

---
+++ app/src/main/java/com/example/a18876311581/coolweather/City.java
@@ -0,0 +1,44 @@
+package com.example.a18876311581.coolweather;
+
+/**
+ * Created by 18876311581 on 2020/8/14.
+ */
+
+public class City {
+    private int id;
+    private String cityName;//市名字
+    private int cityCode;//市代号
+    private int provinceId;//当市所属省id
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCityName() {
+        return cityName;
+    }
+
+    public void setCityName(String cityName) {
+        this.cityName = cityName;
+    }
+
+    public int getCityCode() {
+        return cityCode;
+    }
+
+    public void setCityCode(int cityCode) {
+        this.cityCode = cityCode;
+    }
+
+    public int getProvinceId() {
+        return provinceId;
+    }
+
+    public void setProvinceId(int provinceId) {
+        this.provinceId = provinceId;
+    }
+
+}
---
+++ app/src/main/java/com/example/a18876311581/coolweather/province.java
@@ -0,0 +1,39 @@
+package com.example.a18876311581.coolweather;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by 18876311581 on 2020/8/14.
+ */
+
+public class province extends DataSupport {
+    private int id;
+    private String provinceName;//省名字
+    private int provinceCode;//省代号
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getProvinceName() {
+        return provinceName;
+    }
+
+    public void setProvinceName(String provinceName) {
+        this.provinceName = provinceName;
+    }
+
+    public int getProvinceCode() {
+        return provinceCode;
+    }
+
+    public void setProvinceCode(int provinceCode) {
+        this.provinceCode = provinceCode;
+    }
+
+
+}
